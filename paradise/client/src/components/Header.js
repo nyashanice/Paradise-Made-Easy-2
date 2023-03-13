e56e5286b625abbedff0d362b6b232819cc95f18
Nya Tyus <nyastyus@gmail.com>
2023-03-13 14:43:48 -0400

    create header component

--- paradise/client/src/components/Header.js
+++ paradise/client/src/components/Header.js
@@ -1,2 +1,39 @@
 import React, { useState } from "react";
 import Navigation from "./Navigation";
+import LogIn from "./pages/LogIn";
+import Summary from "./pages/Summary";
+import GuestList from "./pages/GuestList";
+import Dashboard from "./pages/Dashboard";
+
+import("tailwindcss").Config;
+
+export default function Header() {
+  const [currentPage, setCurrentPage] = useState("LogIn");
+
+  const setPage = () => {
+    if (currentPage === "LogIn") {
+      return <LogIn />;
+    }
+    if (currentPage === "Summary") {
+      return <Summary />;
+    }
+    if (currentPage === "GuestList") {
+      return <GuestList />;
+    }
+    if (currentPage === "Dashboard") {
+      return <Dashboard />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <header>
+      <Navigation
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />{" "}
+      {setPage()}
+    </header>
+  );
+}
