2e43d0641644ed118e7606cdccb689b924fd718a
Rodney Major <rodneyjr2001@gmail.com>
2023-03-15 10:24:29 -0400

    login/signup

---
+++ paradise/client/src/components/pages/LogIn.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+export const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: Add server-side validation and error handling
+    if (!email || !password) {
+      setError("Please enter both email and password.");
+      return;
+    }
+    console.log(email);
+    // TODO: Add login logic here
+  };
+
+  return (
+    <div className="auth-form-container">
+      <h2>Login</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="useremail@gmail.com"
+          id="email"
+          name="email"
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="********"
+          id="password"
+          name="password"
+          minLength={8}
+          required
+        />
+        {error && <div className="error">{error}</div>}
+        <button type="submit">Log In</button>
+      </form>
+      <button
+        className="link-btn"
+        onClick={() => props.onFormSwitch("register")}
+      >
+        Don't have an account? Register here.
+      </button>
+    </div>
+  );
+};
