d75b83375b775f18a902856bba434bba9c0f2561
Nya Tyus <nyastyus@gmail.com>
2023-03-15 10:59:18 -0400

    fix errors in terminal

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,35 +1,30 @@
 import React from "react";
-import("tailwindcss").Config;
 
-
-export default function Navigation ({currentPage, handlePageChange}) {
-    return (
-      <nav>
-        <div>
-          <ul>
-            <li>
-              <a href="#home" onClick={() => handlePageChange("Home")}>
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#guestList"
-                onClick={() => handlePageChange("GuestList")}
-              >
-                Guest List
-              </a>
-            </li>
-            <li>
-              <a href="#dashboard" onClick={() => handlePageChange("Dashboard")}>
-                Dashboard
-              </a>
-            </li>
-            <li>
-              <a href="#">Logout</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
+export default function Navigation({ currentPage, handlePageChange }) {
+  return (
+    <nav>
+      <div>
+        <ul>
+          <li>
+            <a href="#home" onClick={() => handlePageChange("Home")}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#guestList" onClick={() => handlePageChange("GuestList")}>
+              Guest List
+            </a>
+          </li>
+          <li>
+            <a href="#dashboard" onClick={() => handlePageChange("Dashboard")}>
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a href="#">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
