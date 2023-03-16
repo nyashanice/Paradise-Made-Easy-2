a58a780c9c9739e684beb7579a11552d94983588
Rodney Major <rodneyjr2001@gmail.com>
2023-03-15 20:54:11 -0400

    updated login

--- client/src/components/pages/LogIn.js
+++ client/src/components/pages/LogIn.js
@@ -27,6 +27,7 @@ const Login = (props) => {
     
     const account = { username: randomUsername, password: randomPassword };
     
+    // useMutation() hook goes here
   }
 
   function LoginPage() {
@@ -37,26 +38,34 @@ const Login = (props) => {
   
     return (
       <div className="auth-form-container bg-gray-100 h-screen flex items-center justify-center">
-      <div className="bg-white rounded-lg shadow-lg p-8 max-w-xs w-full">
-        <h2 className="text-2xl font-bold mb-4">Login</h2>
-        <form className="login-form" onSubmit={handleSubmit}>
-          <fieldset>
-            <div className="mb-4">
-              <label htmlFor="email" className="block text-gray-700 font-bold mb-2">Email:</label>
-              <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" required className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-            </div>
-            <div className="mb-6">
-              <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password:</label>
-              <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" required className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-            </div>
-          </fieldset>
-          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Log In</button>
-        </form>
-        <button className="link-btn text-blue-500 hover:text-blue-700" onClick={() => props.onFormSwitch('register')}>Don't have an account? sign up here.</button>
+        <div className="bg-white rounded-lg shadow-lg p-8 max-w-xs w-full">
+          <h2 className="text-2xl font-bold mb-4">Login</h2>
+          <form className="login-form" onSubmit={handleSubmit}>
+            <fieldset>
+              <div className="mb-4">
+                <label htmlFor="email" className="block text-gray-700 font-bold mb-2">Email:</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" required className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+              </div>
+              <div className="mb-6">
+                <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password:</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" required className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+              </div>
+            </fieldset>
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Log In</button>
+          </form>
+          <button className="link-btn text-blue-500 hover:text-blue-700" onClick={() => props.onFormSwitch('register')}>Don't have an account? sign up here.</button>
+        </div>
       </div>
-    </div>
     );
   }
+
+  const isLoggedIn = Auth.loggedIn();
+
+  return (
+    <div>
+      {isLoggedIn ? <Redirect to="/Home.js" /> : <LoginPage />}
+    </div>
+  );
 }
 
 export default Login;
